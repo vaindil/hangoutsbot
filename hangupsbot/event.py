1f39e693f5e5e0eebe02f1df7df01d90452da111
vaindil <drummer.hall@gmail.com>
2016-01-09 17:47:12 -0500

    disable logging individual chat messages

--- hangupsbot/event.py
+++ hangupsbot/event.py
@@ -82,4 +82,4 @@ class ConversationEvent(GenericEvent):
             logger.log(self.emit_log, 'eid/dt: {}/{}'.format(self.event_id, self.timestamp.astimezone(tz=None).strftime('%Y-%m-%d %H:%M:%S')))
             logger.log(self.emit_log, 'cid/cn: {}/{}'.format(self.conv_id, self.bot.conversations.get_name(self.conv)))
             logger.log(self.emit_log, 'c/g/un: {}/{}/{}'.format(self.user_id.chat_id, self.user_id.gaia_id, self.user.full_name))
-            logger.log(self.emit_log, 'len/tx: {}/{}'.format(len(self.text), self.text))
+            #logger.log(self.emit_log, 'len/tx: {}/{}'.format(len(self.text), self.text))
