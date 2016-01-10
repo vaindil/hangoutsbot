cc5f95a1141f7821eb0425cebde8192bc1394302
vaindil <drummer.hall@gmail.com>
2016-01-09 22:01:43 -0500

    better searching for wmeglossary

--- hangupsbot/plugins/wmeglossary.py
+++ hangupsbot/plugins/wmeglossary.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 from bs4 import BeautifulSoup
-from fuzzywuzzy import fuzz
+import jellyfish
 
 import plugins
 
@@ -18,14 +18,15 @@ def glossary(bot, event, *args):
     for entry in page.find_all('tr', valign='top'):
         child = entry.contents[1]
         for span in child.find_all('span'):
-            tmp = fuzz.ratio(str(span['id']).lower(), spjoin.lower())
-            if tmp >= 80:
-                matches[tmp] = [span['id'], child.b.string]
+            tmp = jellyfish.jaro_winkler(str(span['id']).lower(), spjoin.lower())
+            if tmp >= 0.80:
+                matches[round(tmp*100)] = [span['id'], child.b.string]
+                break
 
         tmpb = entry.contents[1].find('b').string
-        tmp2 = fuzz.ratio(tmpb.lower(), spjoin.lower())
-        if tmp2 >= 80:
-            matches[tmp2] = [tmpb, '<b>' + tmpb + '</b>']
+        tmp2 = jellyfish.jaro_winkler(tmpb.lower(), spjoin.lower())
+        if tmp2 >= 0.80:
+            matches[round(tmp2*100)] = [tmpb, '<b>' + tmpb + '</b>']
 
     if len(matches) == 0:
         yield from bot.coro_send_message(event.conv, "No match found for term: <b>" + spjoin + '</b>')
@@ -42,8 +43,9 @@ def glossary(bot, event, *args):
         if i in matches:
             if count == 4:
                 yield from bot.coro_send_message(event.conv, '<i>(only top 4 matches shown)</i>')
+                return
             count += 1
-            yield from bot.coro_send_message(event.conv, matches[i][1] + ': ' + url + '#' + matches[i][0])
+            yield from bot.coro_send_message(event.conv, '<b>' + matches[i][1] + '</b>: ' + url + '#' + matches[i][0])
 
 
 def _initialise(bot):
