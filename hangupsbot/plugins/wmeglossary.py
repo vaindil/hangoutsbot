d952712de4a1b295dee73a3ca929e0b4ced4fe6f
vaindil <drummer.hall@gmail.com>
2016-01-13 23:09:18 -0500

    fix multi-word glossary lookup

--- hangupsbot/plugins/wmeglossary.py
+++ hangupsbot/plugins/wmeglossary.py
@@ -20,21 +20,23 @@ def glossary(bot, event, *args):
         for span in child.find_all('span'):
             tmp = jellyfish.jaro_winkler(str(span['id']).lower(), spjoin.lower())
             if tmp >= 0.75:
-                matches[round(tmp*100)] = [span['id'], child.b.string]
+                matches[round(tmp * 100)] = [span['id'], child.b.string]
                 break
 
         tmpb = entry.contents[1].find('b').string
         tmp2 = jellyfish.jaro_winkler(tmpb.lower(), spjoin.lower())
         if tmp2 >= 0.75:
-            matches[round(tmp2*100)] = [tmpb, '<b>' + tmpb + '</b>']
+            matches[round(tmp2 * 100)] = [tmpb, '<b>' + tmpb + '</b>']
 
     if len(matches) == 0:
         yield from bot.coro_send_message(event.conv, "No match found for term: <b>" + spjoin + '</b>. The glossary page'
-                                                                                               'is at ' + url)
+                                                                                               ' is at ' + url)
         return
 
     if 100 in matches:
-        yield from bot.coro_send_message(event.conv, '<b>' + matches[100][1] + "</b>: " + url + "#" + matches[100][0])
+        yield from bot.coro_send_message(event.conv,
+                                         '<b>' + matches[100][1] + "</b>: " + url + "#" + matches[100][0].replace(' ',
+                                                                                                                  '_'))
         return
 
     yield from bot.coro_send_message(event.conv, 'No exact matches found for <b>' + spjoin + '</b>. Did you mean:')
@@ -46,7 +48,9 @@ def glossary(bot, event, *args):
                 yield from bot.coro_send_message(event.conv, '<i>(only top 4 matches shown)</i>')
                 return
             count += 1
-            yield from bot.coro_send_message(event.conv, '<b>' + matches[i][1] + '</b>: ' + url + '#' + matches[i][0])
+            yield from bot.coro_send_message(event.conv,
+                                             '<b>' + matches[i][1] + '</b>: ' + url + '#' + matches[i][0].replace(' ',
+                                                                                                                  '_'))
 
 
 def _initialise(bot):
