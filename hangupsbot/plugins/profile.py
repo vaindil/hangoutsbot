3800121ae74113c12a7b45227b392b0a55c3f605
vaindil <drummer.hall@gmail.com>
2016-03-03 19:44:32 -0500

    rename whois to profile, change commands

--- hangupsbot/plugins/profile.py
+++ hangupsbot/plugins/profile.py
@@ -8,38 +8,44 @@ logger = logging.getLogger(__name__)
 
 
 def _initialise(bot):
-    plugins.register_handler(whois, type="message")
+    plugins.register_handler(profile, type="message")
 
 
-def whois(bot, event, *args):
+def profile(bot, event, *args):
     """get information about a user"""
 
     if not isinstance(event.conv_event, hangups.ChatMessageEvent):
         return
 
-    if not event.text.startswith("!whois"):
+    if not event.text.startswith("!profile"):
         return
 
-    if event.text == "!whois":
-        yield from bot.coro_send_message(event.conv, "<b>whois:</b> provide a username to get links to its profiles")
+    if event.text == "!profile" or event.text == "!profiles":
+        yield from bot.coro_send_message(event.conv, "<b>profile:</b> provide a username to get links to its profiles")
+        return
+
+    if event.text.startswith("!profile "):
+        un = event.text[9:].strip()
+    elif event.text.startswith("!profiles "):
+        un = event.text[10:].strip()
+    else:
         return
 
     r = re.compile(r'<.*?>')
-    un = event.text[7:].strip()
     un = r.sub('', un)
 
     if un.isspace() or not un:
-        yield from bot.coro_send_message(event.conv, "<b>whois:</b> provide a username to get links to its profiles")
+        yield from bot.coro_send_message(event.conv, "<b>profile:</b> provide a username to get links to its profiles")
         return
 
     if " " in un:
-        yield from bot.coro_send_message(event.conv, "<b>whois:</b> username cannot contain a space")
+        yield from bot.coro_send_message(event.conv, "<b>profile:</b> username cannot contain a space")
         return
 
     rstr = "<b>Links for " + un + ":</b><br />"
     rstr += "<a href=\"https://www.waze.com/user/editor/" + un + "\">Editor profile</a><br />"
     rstr += "<a href=\"https://www.waze.com/forum/memberlist.php?mode=viewprofile&un=" + un + "\">Forum profile</a><br />"
     rstr += "<a href=\"https://wiki.waze.com/wiki/User:" + un + "\">Wiki profile</a><br />"
-    rstr += "<i>These links will not work if the user does not exist.</i>"
+    rstr += "<i>These links will not work if the profile does not exist.</i>"
 
     yield from bot.coro_send_message(event.conv, rstr)
