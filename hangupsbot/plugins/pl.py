0808c3e69c7069d41f7a822b9c4af32609934dac
vaindil <drummer.hall@gmail.com>
2016-03-23 09:47:13 -0400

    add shorteners, better error handling

--- hangupsbot/plugins/pl.py
+++ hangupsbot/plugins/pl.py
@@ -16,6 +16,7 @@ def pl(bot, event, *args):
     inp = ' '.join(args).strip()
     inter = []
     htc = re.compile('http(?:s)?:\/\/')
+    msgprinted = False
     if not inp:
         yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> no message was provided')
         return
@@ -23,24 +24,52 @@ def pl(bot, event, *args):
     fall = re.findall('(?:http(?:s)?:\/\/)?(?:www\.)?goo\.gl\/\S+', inp)
     if fall:
         for url in fall:
-            if htc.match(url) is None:
-                url = 'https://' + url
-            inp += ' ' + requests.get(url.strip()).url
+            result = checkurl(url, htc)
+            if result == 'BOTTIMEOUTERROR':
+                msgprinted = True
+                yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> error fetching URL ' + url + ' (their fault, not mine!)')
+            else:
+                inp += ' ' + result
 
     fall = re.findall('(?:http(?:s)?:\/\/)?(?:www\.)?j\.mp\/\S+', inp)
     if fall:
         for url in fall:
-            print('URL: ' + url)
-            if htc.match(url) is None:
-                url = 'https://' + url
-            inp += ' ' + requests.get(url.strip()).url
+            result = checkurl(url, htc)
+            if result == 'BOTTIMEOUTERROR':
+                msgprinted = True
+                yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> error fetching URL ' + url + ' (their fault, not mine!)')
+            else:
+                inp += ' ' + result
 
     fall = re.findall('(?:http(?:s)?:\/\/)?(?:www\.)?bit\.ly\/\S+', inp)
     if fall:
         for url in fall:
-            if htc.match(url) is None:
-                url = 'https://' + url
-            inp += ' ' + requests.get(url.strip()).url
+            result = checkurl(url, htc)
+            if result == 'BOTTIMEOUTERROR':
+                msgprinted = True
+                yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> error fetching URL ' + url + ' (their fault, not mine!)')
+            else:
+                inp += ' ' + result
+
+    fall = re.findall('(?:http(?:s)?:\/\/)?(?:www\.)?bit\.do\/\S+', inp)
+    if fall:
+        for url in fall:
+            result = checkurl(url, htc)
+            if result == 'BOTTIMEOUTERROR':
+                msgprinted = True
+                yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> error fetching URL ' + url + ' (their fault, not mine!)')
+            else:
+                inp += ' ' + result
+
+    fall = re.findall('(?:http(?:s)?:\/\/)?(?:www\.)?tinyurl\.com\/\S+', inp)
+    if fall:
+        for url in fall:
+            result = checkurl(url, htc)
+            if result == 'BOTTIMEOUTERROR':
+                msgprinted = True
+                yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> error fetching URL ' + url + ' (their fault, not mine!)')
+            else:
+                inp += ' ' + result
 
     fall = re.findall('(?:http(?:s):\/\/)?(?:www\.|editor-beta\.)?waze\.com\/editor\S+', inp)
     if fall:
@@ -51,7 +80,7 @@ def pl(bot, event, *args):
                 url = 'https://' + url
             inter.append(url)
 
-    if inter is None or not inter:
+    if (inter is None or not inter) and not msgprinted:
         yield from bot.coro_send_message(event.conv_id, '<b>Permalink:</b> no PL found')
         return
 
@@ -71,3 +100,12 @@ def pl(bot, event, *args):
         f = re.sub('&$', '', f)
 
         yield from bot.coro_send_message(event.conv, f.strip())
+
+def checkurl(url, regex):
+    if regex.match(url) is None:
+        url = 'https://' + url
+    try:
+        chk = ' ' + requests.get(url.strip(), timeout=8).url
+    except requests.exceptions.Timeout:
+        chk = 'BOTTIMEOUTERROR'
+    return chk
