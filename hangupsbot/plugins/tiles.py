901bbb9bfd59ffb5e6235e72dbb9125b93b636f9
Steven Hall <vaindil@gmail.com>
2016-06-03 11:31:52 -0400

    fix for new feed categories

--- hangupsbot/plugins/tiles.py
+++ hangupsbot/plugins/tiles.py
@@ -16,8 +16,9 @@ def _initialise(bot):
 
 
 def tiles(bot, event, *args):
-    feedna = feedparser.parse("https://wazestatus.wordpress.com/category/main/north-america-tile-updates/feed/")
-    feedintl = feedparser.parse("https://wazestatus.wordpress.com/category/main/international-tile-updates/feed/")
+    feed = feedparser.parse("https://wazestatus.wordpress.com/category/main/feed/")
+    na = None
+    intl = None
     nadate = None
     intldate = None
     naposteddate = None
@@ -25,37 +26,42 @@ def tiles(bot, event, *args):
 
     reg = re.compile("((January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}, \d{4} \d{2}:\d{2} UTC)")
 
-    for num in range(0, 4):
-        if reg.search(feedna.entries[num].content[0].value) is not None and nadate is None:
-            nadate = reg.search(feedna.entries[num].content[0].value).groups(1)
-            naposteddate = datetime.fromtimestamp(mktime(feedna.entries[num].published_parsed))
-
-        if reg.search(feedintl.entries[num].content[0].value) is not None and intldate is None:
-            intldate = reg.search(feedintl.entries[num].content[0].value).groups(1)
-            intlposteddate = datetime.fromtimestamp(mktime(feedintl.entries[num].published_parsed))
-
-        if nadate is not None and intldate is not None:
+    for num in range(0, 14):
+        if na is None and feed.entries[num].title.startswith('NA'):
+            na = feed.entries[num]
+        if intl is None and feed.entries[num].title.startswith('INTL'):
+            intl = feed.entries[num]
+        if na is not None and intl is not None:
             break
 
-    if nadate is None:
-        nadate = ["(not found)"]
-        naposteddate = datetime(1, 1, 1)
-    if intldate is None:
-        intldate = ["(not found)"]
-        intlposteddate = datetime(1, 1, 1)
+    if na is None:
+        nadate = naposteddate = "N/A"
+    else:
+        if reg.search(na.content[0].value) is not None:
+            nadate = reg.search(na.content[0].value).groups(1)[0]
+            naposteddate = datetime.fromtimestamp(mktime(na.published_parsed)).strftime("%b %d, %Y %H:%M UTC")
+
+    if intl is None:
+        intldate = intlposteddate = "N/A"
+    else:
+        if reg.search(intl.content[0].value) is not None:
+            intldate = reg.search(intl.content[0].value).groups(1)[0]
+            intlposteddate = datetime.fromtimestamp(mktime(intl.published_parsed)).strftime("%b %d, %Y %H:%M UTC")
 
     segments = [hangups.ChatMessageSegment("NA: ", is_bold=True),
-                hangups.ChatMessageSegment(nadate[0]),
+                hangups.ChatMessageSegment(nadate),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("(update performed: ", is_italic=True),
-                hangups.ChatMessageSegment(naposteddate.strftime("%b %d, %Y %H:%M UTC" + ")"), is_italic=True),
+                hangups.ChatMessageSegment(naposteddate, is_italic=True),
+                hangups.ChatMessageSegment(")", is_italic=True),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("INTL: ", is_bold=True),
-                hangups.ChatMessageSegment(intldate[0]),
+                hangups.ChatMessageSegment(intldate),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("(update performed: ", is_italic=True),
-                hangups.ChatMessageSegment(intlposteddate.strftime("%b %d, %Y %H:%M UTC") + ")", is_italic=True),
+                hangups.ChatMessageSegment(intlposteddate, is_italic=True),
+                hangups.ChatMessageSegment(")", is_italic=True),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("\n", hangups.SegmentType.LINE_BREAK),
                 hangups.ChatMessageSegment("Full information: "),
